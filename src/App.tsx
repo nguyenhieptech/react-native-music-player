c2ce1b389be112d765a7747f184cffd2b55809ec
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-02-18 14:47:43 +0700

    Add sorting className plugin

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { AppProvider } from './providers';
 export default function App() {
   return (
     <AppProvider>
-      <View className="flex-1 justify-center items-center">
+      <View className="flex-1 items-center justify-center">
         <Text className="font-primary-600">React Native Misium</Text>
       </View>
     </AppProvider>
