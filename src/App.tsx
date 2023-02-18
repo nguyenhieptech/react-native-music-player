1fe42d63da1643fb6187da5e0bc188ace2948530
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-02-18 14:30:17 +0700

    Add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <AppProvider>
       <View className="flex-1 justify-center items-center">
-        <Text className="font-semibold">React Native Misium</Text>
+        <Text className="font-primary-600">React Native Misium</Text>
       </View>
     </AppProvider>
   );
