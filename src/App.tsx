b39c4c5cf4be873d942dd16dd9c713e7c94664c5
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-02-18 13:10:34 +0700

    Add formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { View, Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>React Native Misium</Text>
     </View>
-  )
+  );
 }
