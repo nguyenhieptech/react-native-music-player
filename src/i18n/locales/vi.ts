2e9f105ea4a2be13222bcb385eea94b5bcc0601d
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-05-12 16:13:46 +0700

    Fix i18n typescript errors

--- src/i18n/locales/vi.ts
+++ src/i18n/locales/vi.ts
@@ -1,6 +1,6 @@
-import { Translations } from './en';
-
-const vi: Translations = {
+// TODO: Add Vietnamese Locale
+// const vi: Translations = {
+const vi = {
   welcome_description: 'Từ những bản nhạc mới nhất đến những bản hit hay nhất, hãy phát các bản nhạc yêu thích của bạn trên Musium ngay bây giờ',
 };
 
